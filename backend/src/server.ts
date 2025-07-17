31a9961d807dc9a29c65e53a4cdb352fc6169cfb
ayushah2002 <astml15@gmail.com>
2025-07-16 22:07:20 -0400

    improved organization

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import app from './app';
+import connectDB from './database/connectDB';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+connectDB().then(() => {
+    app.listen(4000, () => {
+        console.log('Server running on port 4000', process.env.DB_NAME);
+    })
+    })
+    .catch((error) => {
+        console.error('Failed to start server', error);
+        process.exit(1);
+    })
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,33 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-      "@typescript-eslint/no-explicit-any": "off"
-    }
-  }  
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ]);
